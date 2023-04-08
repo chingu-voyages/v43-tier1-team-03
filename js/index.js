e788ab14b332de6266befa31a9fd86c893c2e129
Cristina Vuculescu <vuculescucristina@gmail.com>
2023-04-08 18:38:31 +0300

    made some changes in how input and story work

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
-function redirect() {
-    console.log('clicked');
-}
+const switchState = document.querySelectorAll('.switch');
+const alienNameHolders = document.querySelectorAll('.alien-name');
+const errors = document.querySelectorAll('.error');
+const alienNameInput = document.getElementById('alien-name-input');
+const btnSubmit = document.getElementById('btn-submit');
+
+
+
+
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if(alienNameInput.value != '') {
+        for (let alienName of alienNameHolders) {
+            alienName.innerHTML = alienNameInput.value;
+        }
+        switchState[0].classList.remove('active');
+        switchState[1].classList.add('active');
+    }
+    else {
+        for (let error of errors) {
+            error.innerHTML = "Field required";
+        }
+    }
+})
+
