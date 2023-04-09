3e49c5c574e6b0b2f5a9472d4220ee1204dd6c17
Cristina Vuculescu <vuculescucristina@gmail.com>
2023-04-09 17:47:54 +0300

    finished Xirxians story and inputs

--- js/index.js
+++ js/index.js
@@ -3,11 +3,18 @@ const alienNameHolders = document.querySelectorAll('.alien-name');
 const nounOneHolders = document.querySelectorAll('.noun-one');
 const nounTwoHolders = document.querySelectorAll('.noun-two');
 const nounThreeHolders = document.querySelectorAll('.noun-three');
-const errors = document.querySelectorAll('.error');
+const adjectiveOneHolders = document.querySelectorAll('.adjective-one');
+const ajectiveTwoHolder = document.getElementById('adjective-two');
+const verbOneHolder = document.getElementById('verb-one');
+const verbTwoHolder = document.getElementById('verb-two');
 const alienNameInput = document.getElementById('alien-name-input');
 const nounOneInput = document.getElementById('noun1');
 const nounTwoInput = document.getElementById('noun2');
 const nounThreeInput = document.getElementById('noun3');
+const adjectiveOneInput = document.getElementById('adjective1');
+const adjectiveTwoInput = document.getElementById('adjective2');
+const verbOneInput = document.getElementById('verb1');
+const verbTwoInput = document.getElementById('verb2');
 const btnSubmit = document.getElementById('btn-submit');
 
 
@@ -29,6 +36,14 @@ btnSubmit.addEventListener('click', (e) => {
         for (let nounThree of nounThreeHolders) {
             nounThree.innerHTML = nounThreeInput.value;
         }
+        for (let adjectiveOne of adjectiveOneHolders) {
+            adjectiveOne.innerHTML = adjectiveOneInput.value;
+        }
+
+        ajectiveTwoHolder.innerHTML = adjectiveTwoInput.value;
+        verbOneHolder.innerHTML = verbOneInput.value;
+        verbTwoHolder.innerHTML = verbTwoInput.value;
+
         switchState[0].classList.remove('active');
         switchState[1].classList.add('active');
     }
