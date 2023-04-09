41af98a55ca346540af1c9384f5957fd7552741e
Cristina Vuculescu <vuculescucristina@gmail.com>
2023-04-09 15:16:15 +0300

    added extra inputs

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 const switchState = document.querySelectorAll('.switch');
 const alienNameHolders = document.querySelectorAll('.alien-name');
+const nounOneHolders = document.querySelectorAll('.noun-one');
+const nounTwoHolders = document.querySelectorAll('.noun-two');
+const nounThreeHolders = document.querySelectorAll('.noun-three');
 const errors = document.querySelectorAll('.error');
 const alienNameInput = document.getElementById('alien-name-input');
+const nounOneInput = document.getElementById('noun1');
+const nounTwoInput = document.getElementById('noun2');
+const nounThreeInput = document.getElementById('noun3');
 const btnSubmit = document.getElementById('btn-submit');
 
 
@@ -13,6 +19,15 @@ btnSubmit.addEventListener('click', (e) => {
     if(alienNameInput.value != '') {
         for (let alienName of alienNameHolders) {
             alienName.innerHTML = alienNameInput.value;
+        }
+        for (let nounOne of nounOneHolders) {
+            nounOne.innerHTML = nounOneInput.value;
+        }
+        for (let nounTwo of nounTwoHolders) {
+            nounTwo.innerHTML = nounTwoInput.value;
+        }
+        for (let nounThree of nounThreeHolders) {
+            nounThree.innerHTML = nounThreeInput.value;
         }
         switchState[0].classList.remove('active');
         switchState[1].classList.add('active');
