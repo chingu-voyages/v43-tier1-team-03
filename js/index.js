ca2a7832661216e16330fce76c0bac85b5eab5b1
Cristina Vuculescu <vuculescucristina@gmail.com>
2023-04-14 19:11:23 +0300

    fixes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 const closeBtn = document.getElementById('close');
 const switchStates = document.querySelectorAll('.switch');
+const inputsEl = documents.querySelectorAll('.inputs');
 
 closeBtn.addEventListener('click', function() {
     switchStates[1].classList.remove('active');
     switchStates[0].classList.add('active');
+    for (input of inputsEl) {
+        input.value = '';
+    }
 })
 
 
